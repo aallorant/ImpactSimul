02b0edceffff628a5d67c028fcaa1c6deaeb96a0
aallorant <43590555+aallorant@users.noreply.github.com>
2020-11-05 09:29:26 -0800

    Update main.R

--- R/main.R
+++ R/main.R
@@ -54,7 +54,7 @@ init <- init(i.prev.male = 1,
 
 for(scenario_simulation in names(list_scenarios)){
   print(scenario_simulation)
-  assign(paste0("result_", scenario_simulation), run_simulations(init, param, scenario_simulation, intervention_start = 0, prop_male, nsteps = 52 * 7, nsim = 100))
+  assign(paste0("result_", scenario_simulation), run_simulations(init, param, scenario_simulation, intervention_start = 0, prop_male, nsteps = 52 * 5, nsim = 100))
 }
 
 ## Simulation results for the different scenarios
